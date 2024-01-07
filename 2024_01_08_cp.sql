46373085c83adc99d8d89d958c0c750d78d9b326
taekhyeon-sofit <taekhyeon.nam@sofit.kr>
2024-01-08 00:08:57 +0900

    CP 공구 관련 쿼리 추가

---
+++ 2024_01_08_cp.sql
@@ -0,0 +1,15 @@
+select created_at,
+       count,
+       history,
+       status,
+       product_id,
+       product_option_ids,
+       product_options,
+       user_id,
+       price,
+       coupon_discount_price,
+       coupon_id
+from order_detail
+where product_id in (6847, 2366, 6676, 6673, 2368, 4314, 4317)
+  and created_at >= '2024-01-08 00:00:00'
+order by created_at desc;
